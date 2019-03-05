fbee5af482b367ee40473289be003cdcd7bcae2f
GarretR <garret.rueckert.guitar@gmail.com>
2019-03-04 19:59:38 -0700

    books api routes

---
+++ routes/book-api-routes.js
@@ -0,0 +1,36 @@
+var db = require("../models");
+var passport = require("../config/passport");
+
+module.exports = function (app) {
+    app.get("/api/:userid/books/", function (req, res) {
+        db.Book.findAll({});
+    });
+
+    app.post("api/create/book", function (req, res) {
+        db.Book.create({
+            title: req.body.title
+        }).then (function (dbBook) {
+            res.json(dbBook);
+        });
+    });
+
+    app.put("api/update/book/:id", function (req, res) {
+        db.Book.update({
+            where: {
+                title: req.body.title
+            }
+        }).then(function (dbBook) {
+            res.json(dbBook);
+        });
+    });
+
+    app.delete("api/delete/book/:id", function (req, res) {
+        db.Book.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then (function (dbBook) {
+            res.json(dbBook);
+        });
+    });
+};
